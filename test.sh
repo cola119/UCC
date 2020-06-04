0222ce946900a76d67964cc64e04234b755733c6
cola119 <k.cola119@ezweb.ne.jp>
2020-06-04 23:43:36 +0900

    basic assemble and test runner

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./ucc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but go $actual"
+    fi
+}
+
+assert 0 0
+assert 41 41
+
+echo OK
